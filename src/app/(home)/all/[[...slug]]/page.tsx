a18feb9cd0c75488e3971ced44ece4d443512f2d
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-02-26 08:01:14 +0900

    Implemented pagination

--- src/app/(home)/all/[[...slug]]/page.tsx
+++ src/app/(home)/all/[[...slug]]/page.tsx
@@ -1,15 +1,28 @@
 import Link from 'next/link';
 import { getPages } from '@/app/source';
+import { notFound } from 'next/navigation';
+import { Pagination } from '../_components/Pagination';
 
-export default function HomePage() {
-  const pages = getPages().sort(
-    (a, b) => (b.data.exports.lastModified ?? 0) - (a.data.exports.lastModified ?? 0),
-  );
+const totalPosts = getPages().length;
+const postsPerPage = 5;
+const pageCount = Math.ceil(totalPosts / postsPerPage);
+
+export default function HomePage({ params }: { params: { slug?: string[] } }) {
+  const pageIndex = params.slug ? parseInt(params.slug[0], 10) - 1 : 0;
+  const startIndex = pageIndex * postsPerPage;
+  const endIndex = startIndex + postsPerPage;
+  const pages = getPages()
+    .sort((a, b) => (b.data.exports.lastModified ?? 0) - (a.data.exports.lastModified ?? 0))
+    .slice(startIndex, endIndex);
+
+  if (startIndex >= totalPosts) notFound();
 
   return (
     <main>
       <div className='mx-4 my-6 lg:mx-auto lg:w-[992px]'>
-        <h2 className='text-3xl font-bold my-4'>全記事一覧({pages.length})</h2>
+        <h2 className='text-3xl font-bold my-4'>
+          全{totalPosts}記事一覧({startIndex + 1}~{endIndex < totalPosts ? endIndex : totalPosts})
+        </h2>
         <div className='flex flex-col gap-4 text-left'>
           {pages.map((page, i) => {
             const lastModified = page.data.exports.lastModified;
@@ -32,7 +45,18 @@ export default function HomePage() {
             );
           })}
         </div>
+        <div className='my-6'>
+          <Pagination current={pageIndex + 1} end={pageCount} path='/all' />
+        </div>
       </div>
     </main>
   );
 }
+
+export const generateStaticParams = async () => {
+  const slugs = Array.from({ length: pageCount }, (_, index) => ({
+    params: { slug: [(index + 1).toString()] },
+  }));
+
+  return slugs;
+};
---
+++ src/app/(home)/all/_components/Pagination.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import { ReactNode } from 'react';
+import { ChevronRight, ChevronLeft, ChevronsRight, ChevronsLeft } from 'lucide-react';
+
+const Card = ({
+  children,
+  num,
+  path,
+  disable,
+}: {
+  children?: ReactNode;
+  num: number;
+  path: string;
+  disable: boolean | undefined;
+}) => {
+  if (disable) {
+    return <div className='rounded-full border bg-card p-2 text-muted-foreground'>{children}</div>;
+  }
+  return (
+    <Link
+      href={path + '/' + num.toString()}
+      className='rounded-full border bg-card p-2 text-card-foreground transition-colors hover:bg-accent hover:text-accent-foreground'
+    >
+      {children}
+    </Link>
+  );
+};
+
+export const Pagination = ({
+  current,
+  end,
+  path,
+}: {
+  current: number;
+  end: number;
+  path: string;
+}) => {
+  return (
+    <div className='flex justify-center gap-2'>
+      <Card num={1} path={path} disable={current === 1}>
+        <h2 className='text-xl font-medium'>
+          <ChevronsLeft />
+        </h2>
+      </Card>
+      <Card num={current - 1} path={path} disable={current === 1}>
+        <h2 className='text-xl font-medium'>
+          <ChevronLeft />
+        </h2>
+      </Card>
+      <div className='my-auto text-2xl'>
+        {current} / {end}
+      </div>
+      <Card num={current + 1} path={path} disable={current === end}>
+        <h2 className='text-xl font-medium'>
+          <ChevronRight />
+        </h2>
+      </Card>
+      <Card num={end} path={path} disable={current === end}>
+        <h2 className='text-xl font-medium'>
+          <ChevronsRight />
+        </h2>
+      </Card>
+    </div>
+  );
+};
