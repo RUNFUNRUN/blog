5cd88a25739bf80d6db8c7ac1e7d0551403bc64d
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-01-16 23:35:13 +0900

    fix: top page num of articles from 5 to 3

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,11 +5,11 @@ import { PostCard } from './_components/post-card';
 const HomePage = () => {
   const posts = getPages()
     .toSorted((a, b) => b.data.date.getTime() - a.data.date.getTime())
-    .slice(0, 5);
+    .slice(0, 3);
 
   return (
-    <main className='mx-4 my-12 lg:mx-auto lg:w-[992px]'>
-      <div className='mb-6 text-2xl font-bold'>
+    <main className='mx-4 my-12 space-y-12 lg:mx-auto lg:w-[992px]'>
+      <div className='text-2xl font-bold'>
         <p>
           RUNFUNRUNのブログです。プロフィールは
           <InlineLink href='https://runfun.run' blank>
@@ -28,26 +28,26 @@ const HomePage = () => {
           また、記事には誤りがある可能性があります。もし見つけた場合は連絡またはIssue頂けると幸いです。
         </p>
       </div>
-      <div>
-        <p className='text-2xl sm:text-3xl font-bold my-4'>
+      <div className='space-y-4'>
+        <p className='text-2xl sm:text-3xl font-bold'>
           最新の記事 (<InlineLink href='/list'>全記事はこちら</InlineLink>)
         </p>
-      </div>
-      <div className='flex flex-col gap-4 text-left'>
-        {posts.map((post) => {
-          const date = new Date(post.data.date).toLocaleDateString('ja-JP', {
-            timeZone: 'Asia/Tokyo',
-          });
-          return (
-            <PostCard
-              title={post.data.title}
-              description={post.data.description ?? ''}
-              url={post.url}
-              date={date}
-              key={post.url}
-            />
-          );
-        })}
+        <div className='flex flex-col gap-4 text-left'>
+          {posts.map((post) => {
+            const date = new Date(post.data.date).toLocaleDateString('ja-JP', {
+              timeZone: 'Asia/Tokyo',
+            });
+            return (
+              <PostCard
+                title={post.data.title}
+                description={post.data.description ?? ''}
+                url={post.url}
+                date={date}
+                key={post.url}
+              />
+            );
+          })}
+        </div>
       </div>
     </main>
   );
