01545e2a6d26a5e09cc63e80f928c3ae9b596b47
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-01-19 01:28:50 +0900

    fix: 404 page header

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,23 @@
+import { Header } from '@/components/header';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import { getLinks } from 'fumadocs-ui/layouts/shared';
 import Image from 'next/image';
 import { baseOptions, linkItems } from './layout.config';
 
 const NotFound = () => {
   return (
-    <HomeLayout {...baseOptions} links={linkItems}>
+    <HomeLayout
+      {...baseOptions}
+      links={linkItems}
+      nav={{
+        component: (
+          <Header
+            finalLinks={getLinks(linkItems, baseOptions.githubUrl)}
+            {...baseOptions}
+          />
+        ),
+      }}
+    >
       <div className='text-center'>
         <p className='my-4 text-4xl font-bold'>404 Not Found</p>
         <p className='text-lg'>
