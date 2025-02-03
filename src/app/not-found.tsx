fae8bf62fbc465f5aea17746c7d8e1bed87f104f
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-02-03 13:03:47 +0900

    fix: not foundページに文章追加

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/header';
+import { InlineLink } from '@/components/inline-link';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { getLinks } from 'fumadocs-ui/layouts/shared';
 import Image from 'next/image';
@@ -21,15 +22,26 @@ const NotFound = () => {
       <div className='my-12 space-y-4 text-center'>
         <p className='text-4xl font-bold'>404 Not Found</p>
         <p className='text-lg'>
-          代わりに吉村家のラーメンの飯テロをお送りします。
+          <span className='block md:inline'>
+            記事のURLが変更された可能性があります。
+          </span>
+          <span className='block md:inline'>
+            お手数ですが、<InlineLink href='/list'>記事一覧</InlineLink>
+            をご確認ください。
+          </span>
         </p>
-        <Image
-          src='/ramen.jpg'
-          width={800}
-          height={600}
-          alt='yoshimueraya'
-          className='mx-auto'
-        />
+        <div>
+          <p className='text-lg'>
+            代わりに吉村家のラーメンの飯テロをお送りします。
+          </p>
+          <Image
+            src='/ramen.jpg'
+            width={800}
+            height={600}
+            alt='yoshimueraya'
+            className='mx-auto'
+          />
+        </div>
       </div>
     </HomeLayout>
   );
