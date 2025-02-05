7c612f8ad230efff876252b807056dae071c1ba9
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-02-05 23:54:54 +0900

    fix: tag card style

--- src/app/(home)/tags/page.tsx
+++ src/app/(home)/tags/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
       <h1 className='text-3xl font-bold mb-6'>タグ一覧</h1>
       <div className='flex gap-2 flex-wrap'>
         {tags.map((tag) => (
-          <TagCard name={tag} key={tag} />
+          <TagCard name={tag} displayCount={true} key={tag} />
         ))}
       </div>
     </div>
--- src/components/tag-card.tsx
+++ src/components/tag-card.tsx
@@ -2,17 +2,22 @@ import { getPostsByTag } from '@/lib/source';
 import { TagIcon } from 'lucide-react';
 import Link from 'next/link';
 
-export const TagCard = ({ name }: { name: string }) => {
+export const TagCard = ({
+  name,
+  displayCount = false,
+}: { name: string; displayCount?: boolean }) => {
   const numOfPosts = getPostsByTag(name);
 
   return (
     <Link
       href={`/tags/${name}`}
-      className='rounded-lg border bg-fd-card text-fd-card-foreground shadow-md transition-colors hover:bg-fd-accent/80 flex gap-1 no-underline text-lg p-1'
+      className='rounded-md border bg-fd-card text-fd-card-foreground shadow-md transition-colors hover:bg-fd-accent/80 flex gap-1 no-underline text-lg px-2 py-0.5'
     >
       <TagIcon size={18} className='my-auto text-fd-muted-foreground' />
       <span className='text-fd-card-foreground'>{name}</span>
-      <span className='text-fd-muted-foreground'>({numOfPosts.length})</span>
+      {displayCount && (
+        <span className='text-fd-muted-foreground'>({numOfPosts.length})</span>
+      )}
     </Link>
   );
 };
